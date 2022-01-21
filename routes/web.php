93191fdaf337064d9f57c2fe89564cc82a88bf0d
BrayanMaychuc <brayanmay137@gmail.com>
2022-01-20 22:12:05 -0600

    Instalación de fpdf
    Realizamos la instalación del plugin y se realizo la prueba

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('pdf', function (Codedge\Fpdf\Fpdf\Fpdf $fpdf) {
+
+    $fpdf->AddPage();
+    $fpdf->SetFont('Courier', 'B', 18);
+    $fpdf->Cell(50, 25, 'Hello World!');
+    $fpdf->Output();
+    exit;
+
+});
